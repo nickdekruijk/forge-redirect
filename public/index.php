c14c4140ac824b436ab69a8d025f9bddd7d89ffb
Nick de Kruijk <git@nickdekruijk.nl>
2019-04-18 14:25:13 +0200

    Added some code comments

--- public/index.php
+++ public/index.php
@@ -4,10 +4,11 @@
     $dotenv = Dotenv\Dotenv::create(__DIR__ . '/..');
     $dotenv->load();
 
-    //
+    // Get the .env variables
     $domain = rtrim(getenv('REDIRECT_TO'), '/');
     $append = filter_var(getenv('REDIRECT_WITH_REQUEST_URI'), FILTER_VALIDATE_BOOLEAN);
 
+    // Build the new url to redirect to
     $new_url = rtrim($domain, '/');
     if ($append) {
         $new_url .= getenv('REQUEST_URI');
