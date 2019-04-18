3b97d6862f0f755288868ad51a1b3b4525967bfe
Nick de Kruijk <git@nickdekruijk.nl>
2019-04-18 14:24:56 +0200

    Cleanup code used when debugging

--- public/index.php
+++ public/index.php
@@ -1,6 +1,4 @@
 <?php
-    header('Content-type: text/plain');
-
     // Initialize composer autoload and Dotenv package
     require __DIR__ . '/../vendor/autoload.php';
     $dotenv = Dotenv\Dotenv::create(__DIR__ . '/..');
@@ -9,7 +7,6 @@
     //
     $domain = rtrim(getenv('REDIRECT_TO'), '/');
     $append = filter_var(getenv('REDIRECT_WITH_REQUEST_URI'), FILTER_VALIDATE_BOOLEAN);
-    $request = getenv('REQUEST_URI');
 
     $new_url = rtrim($domain, '/');
     if ($append) {
